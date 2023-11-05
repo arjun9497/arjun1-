1f509c587c29b92795a9abae0417e35a1feeb8c0
s arjun <sarjun20037@gmail.com>
2023-11-05 11:04:56 +0530

    first commit

---
+++ ceaser.py
@@ -0,0 +1,38 @@
+def ceaser(Plaintext,key):
+    ct = ' '
+    for i in range(len(Plaintext)):
+        c = Plaintext[i]
+        if c == ' ':
+            ct += ' '
+        elif(c.isupper()):
+            ct += chr((ord(c)+key-65)%26+65)
+            
+        else:
+            ct += chr((ord(c)+key-97)%26+97)
+           
+    
+    return ct
+    
+    
+pt = input("ente the string too encrypt:")
+k = int(input("enter the key value:"))
+enc = ceaser(pt,k)
+print('the encryptedd value is:',enc)
+
+def decrypt(ciphertext,key):
+    pt = ' '
+    for i in range(len(ciphertext)):
+        c = ciphertext[i]
+        if c == ' ':
+            pt += ' '
+        elif(c.isupper()):
+            pt += chr((ord(c)+key-65)%26+65)
+            
+        else:
+            pt += chr((ord(c)+key-97)%26+97)
+           
+    
+    return pt
+message = (enc,k)
+print("the decryption key hrishi:",message)
+
